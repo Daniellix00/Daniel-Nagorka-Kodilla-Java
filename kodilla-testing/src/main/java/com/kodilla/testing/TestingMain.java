3cdefc19a77ddb80740e2718154637672dc95724
Daniel Nagorka <danielosnag1996@gmail.com>
2023-10-12 19:23:53 +0200

    Calculator 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
-
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain{
-        public static void main(String[] args) {
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-        }
+    public static void main(String[] args) {
+Calculator calculator = new Calculator();
+Integer result = calculator.add(10,5);
+if (result.equals(15))
+{
+    System.out.println("test ok");
+}
+else
+{
+    System.out.println("Error!");
+}
+Integer result2 = calculator.subtract(10,5);
+if(result2.equals(5))
+{
+    System.out.println("test ok");
+}
+else
+{
+    System.out.println("Error!");
+}
+    }
+
 }
