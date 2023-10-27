4ace7cc7ebf47776a1aee38913bc719b0b72b7e9
Daniel Nagorka <danielosnag1996@gmail.com>
2023-10-27 20:42:18 +0200

    lambda

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -2,6 +2,8 @@ package com.kodilla.stream.beautifier;
 
 public class PoemBeautifier {
     public void beautify (String text, PoemDecorator poemDecorator){
-        poemDecorator.decorate(text);
+
+     String beautifiedText =   poemDecorator.decorate(text);
+     System.out.println("Upiekszony tekst: "+ beautifiedText );
     }
 }
