1ac03b5c331f4a38dc1b5cead95ddef72fedd45e
Daniel Nagorka <danielosnag1996@gmail.com>
2023-11-02 19:07:55 +0100

    Array

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.array;
+
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+   static double getAverage(int[]numbers){
+       IntStream.range(1, numbers.length+1)
+               .forEach(i -> System.out.println(numbers[i-1]));
+       double srednia = IntStream.range(1,numbers.length+1)
+               .average()
+               .orElse(0.0);
+
+
+       return srednia;
+
+
+       }
+   }
+
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    void testGetAverage(){
+        //Given
+    int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+    //When
+    double srednia = ArrayOperations.getAverage(numbers);
+    //then
+     assertEquals(10.5,srednia, 0.01);
+}}
